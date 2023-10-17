ce5da77b34f24a81c78932dcd1f516ae5f1cecf7
Hayden Price <hwp@arizona.edu>
2023-10-17 01:49:45 +0000

    rough. needs serious style refactoring and portfolio work

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -1,41 +1,41 @@
 import React, { useState, useEffect } from "react";
+import marked from "marked"; // import the marked library
+import DOMPurify from "dompurify"; // import the DOMPurify library
 import image from "../images/middle.png";
+import "../MarkdownStyles.css"; // Assuming you store your scoped styles in this file.
 
 const imageAltText = "purple and blue lofi illustrated background";
 
 const About = () => {
-  // State to hold the fetched readme content
   const [readmeContent, setReadmeContent] = useState("");
 
-  // Fetch the raw content of the README.md file from GitHub
   useEffect(() => {
     fetch("https://raw.githubusercontent.com/hpwn/hpwn/main/README.md")
-      .then((response) => response.text()) // Getting the response body as plain text
+      .then((response) => response.text())
       .then((content) => {
-        // Setting the state to the fetched text content
-        setReadmeContent(content);
+        const sanitizedHtml = DOMPurify.sanitize(marked(content)); // Convert markdown to HTML and sanitize it
+        setReadmeContent(sanitizedHtml);
       })
       .catch((error) => {
         console.error("Error fetching README.md: ", error);
         setReadmeContent("Error loading content. Please check the console for more details.");
       });
-  }, []); // The empty array means this effect runs once when the component mounts
+  }, []);
 
   return (
     <section className="min-height shadow-effect" id="about">
       <img className="background" src={image} alt={imageAltText} />
       <div
+        className="markdown-content-wrapper" // Add a class to the wrapper
         style={{
           backgroundColor: "#D3D3D3",
-          width: "30%",
+          width: "40%", // Adjust width if necessary
           padding: "4rem",
           margin: "3rem auto",
-          textAlign: "left", // Align the text to the left
+          textAlign: "center",
         }}
       >
-        <h2>About Me</h2>
-        {/* Display the content as preformatted text */}
-        <pre style={{ whiteSpace: "pre-wrap", wordBreak: "break-word" }}>{readmeContent}</pre>
+        <div dangerouslySetInnerHTML={{ __html: readmeContent }} />
       </div>
     </section>
   );
