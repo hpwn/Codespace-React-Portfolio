df4d0f941fd42d1de36b5e518b3e352fa3b8adb4
Hayden Price <hwp@arizona.edu>
2023-10-28 18:29:41 +0000

    update sub link

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -15,7 +15,7 @@ const Home = ({ name, title }) => {
           <a href="#about">About</a>
           <a href="#footer">Contact</a>
           {/* You can add a "Subscribe" link here if you have a specific destination in mind */}
-          <a href="https://twitch.tv/hp_az">Subscribe</a>
+          <a href="https://hwp.lol">Subscribe</a>
         </div>
         {/* Removed the arrow icon from here */}
       </div>
